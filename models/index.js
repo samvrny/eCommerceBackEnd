7ba630278938772e06840d08fe9336ef1188b414
samvrny <samvrny@gmail.com>
2022-08-20 19:43:39 -0500

    got the first route up and running

--- models/index.js
+++ models/index.js
@@ -17,13 +17,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'first_tag',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'first_tag',
   foreignKey: 'tag_id'
 })
 
