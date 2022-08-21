74d766b3add0f4d4ab0af64d32c7d35c5ca0bfb3
samvrny <samvrny@gmail.com>
2022-08-20 19:32:44 -0500

    created first route

--- models/index.js
+++ models/index.js
@@ -27,9 +27,4 @@ Tag.belongsToMany(Product, {
   foreignKey: 'tag_id'
 })
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,9 +2,22 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Products
 });
 
