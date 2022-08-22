cb6fbbddb4f4f685e0ae02eb23b897d2e93192b4
samvrny <samvrny@gmail.com>
2022-08-22 18:59:12 -0500

    got the delete product route working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -134,8 +134,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.sendStatus(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
