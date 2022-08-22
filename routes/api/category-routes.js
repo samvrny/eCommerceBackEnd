b255cdcf87293c19682ab311f29fa3c2fb5615e0
samvrny <samvrny@gmail.com>
2022-08-21 21:41:23 -0500

    finished category delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -81,8 +81,24 @@ router.put('/:id', (req, res) => {
   });
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.status(404).json({ message: 'No category found with this id' })
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
